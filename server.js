b390253d6c6652084ddcd42b24d27e3a17009751
Jack <jacktiebie45@gmail.com>
2022-05-11 19:09:48 +0200

    Require other file for math

---
+++ server.js
@@ -0,0 +1,8 @@
+const os = require('os')
+const path = require('path')
+
+const {add, multiply, subtract } = require('./math')
+
+console.log(add(2,3))
+console.log(multiply(2,3))
+console.log(subtract(2,3))
