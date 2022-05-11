9bbfefb3cc290e513bd8d9f014890f608ffb2d52
Jack <jacktiebie45@gmail.com>
2022-05-11 20:12:10 +0200

    Read and Write files - 1

--- server.js
+++
@@ -1,8 +0,0 @@
-const os = require('os')
-const path = require('path')
-
-const {add, multiply, subtract } = require('./math')
-
-console.log(add(2,3))
-console.log(multiply(2,3))
-console.log(subtract(2,3))
